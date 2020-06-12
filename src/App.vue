4707b484d3657970af335ad0516e21f84935457b
turinglabsorg <turinglabs@criptext.com>
2020-06-12 10:59:25 +0200

    backup process

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div v-if="!isLogging && wallet">
+    <div v-if="!isLogging && wallet && backup">
       <b-navbar>
         <template slot="brand">
           <b-navbar-item tag="router-link" :to="{ path: '/' }">
@@ -35,6 +35,24 @@
       <br />
       <br />
     </div>
+
+    <div v-if="!isLogging && wallet && !backup">
+      <section class="hero">
+        <div class="hero-body" style="padding: 0;">
+          <div class="container" id="backup" style="margin-top:50px;">
+            <div class="card">
+              <div style="padding: 50px 20px;">
+                <h1 class="title is-1">Fai un backup della tua identità</h1>
+                <p>Per prevenire la perdità di dati è fondamentale fare un backup sicuro di questo file.</p><br>
+                <b-button v-on:click="downloadBackup" type="is-primary" size="is-large">ESEGUI BACKUP</b-button>
+                <a id="downloadid" style="display:none"></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+
     <div v-if="!wallet">
       <section class="hero">
         <div class="hero-body" style="padding: 0;">
@@ -46,7 +64,7 @@
                 <h2 class="subtitle">
                   <br />Hai bisogno un'identità Scrypta per entrare in piattaforma.<br>Puoi entrare grazie al tuo .sid, se lo possiedi, oppure puoi creare una nuova identità ora.
                   <br />
-                  <br />Use <a href="https://id.scryptachain.org/" target="_blank">Scrypta ID Extension</a> or <a v-on:click="showCreate">create a new wallet</a>.
+                  <br />Usa <a href="https://id.scryptachain.org/" target="_blank">Scrypta ID Extension</a> o <a v-on:click="showCreate">crea un nuovo wallet</a>.
                   <br />
                   <br />
                   <b-upload v-model="file" v-on:input="loadWalletFromFile" drag-drop>
@@ -71,6 +89,7 @@
         </div>
       </section>
     </div>
+
     <b-loading :is-full-page="true" :active.sync="isLogging" :can-cancel="false"></b-loading>
     <b-modal :active.sync="showCreateModal" has-modal-card trap-focus aria-role="dialog" aria-modal>
       <form action>
@@ -128,6 +147,7 @@ export default {
       isLogging: true,
       file: [],
       isCreating: false,
+      backup: false,
       isUpdating: false,
       showCreateModal: false,
       password: "",
@@ -143,6 +163,10 @@ export default {
       app.address = SIDS[0];
       let identity = await app.scrypta.returnIdentity(app.address);
       app.wallet = identity;
+      let check_backup = localStorage.getItem('sid_backup')
+      if(check_backup !== null && check_backup !== undefined && check_backup === app.address){
+        app.backup = true
+      }
       app.isLogging = false;
     } else {
       app.isLogging = false;
@@ -166,7 +190,9 @@ export default {
             let key = await app.scrypta.readKey(password, dataKey);
             if (key !== false) {
               app.scrypta.importPrivateKey(key.prv, password);
-              localStorage.setItem("SID", dataKey);
+              localStorage.setItem("SID", dataKey)
+              let exp = dataKey.split(':')
+              localStorage.setItem("sid_backup", exp[0])
               location.reload();
             } else {
               app.$buefy.toast.open({
@@ -225,6 +251,42 @@ export default {
           type: "is-danger"
         });
       }
+    },
+    downloadBackup(){
+      const app = this
+      app.$buefy.dialog.prompt({
+        message: `Enter wallet password`,
+        inputAttrs: {
+          type: "password"
+        },
+        trapFocus: true,
+        onConfirm: async password => {
+          let SID = localStorage.getItem('SID')
+          let key = await app.scrypta.readKey(password, SID);
+          if (key !== false) {
+            var a = document.getElementById("downloadid");
+            app.backup = true
+            localStorage.setItem('sid_backup', app.address)
+            var file = new Blob(
+              [SID],
+              { type: "sid" }
+            );
+            a.href = URL.createObjectURL(file);
+            a.download = app.address + ".sid";
+            var clickEvent = new MouseEvent("click", {
+              view: window,
+              bubbles: true,
+              cancelable: false
+            });
+            a.dispatchEvent(clickEvent);
+          }else{
+            app.$buefy.toast.open({
+              message: "Wrong password!",
+              type: "is-danger"
+            });
+          }
+        }
+      })
     }
   }
 };
